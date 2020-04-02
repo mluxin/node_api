267ee7c42145e0d982ef1e0f1e5d4c296364c150
Rinaroar <marina.luxin@gmail.com>
2020-04-02 17:44:39 +0200

    added server.js + gitignore + html

---
+++ README.md
@@ -0,0 +1,10 @@
+NODEapi_boilerplate
+Code de départ pour mettre en place une API NodeJS.
+
+Configuration
+Etapes à suivre :
+
+Créer un fichier .env a la racine du serveur en suivant le modèle .env.dist.
+Installer les dépendances avec la commande npm i
+Innstaller - si ce n'est déjà fait - NodeMon avec la commande sudo npm i -g nodemon
+Lancer le serveur avec la commande npm start
--- server.js
+++ server.js
@@ -1,1 +1,60 @@
-console.log('hello JS');
+/*
+Import
+*/
+
+ // NodeJS
+  require('dotenv').config(); //=> https://www.npmjs.com/package/dotenv
+  const express = require('express'); //=> https://www.npmjs.com/package/express
+  const bodyParser = require('body-parser'); //=> https://www.npmjs.com/package/body-parser
+  const cookieParser = require('cookie-parser'); //=> https://www.npmjs.com/package/cookie-parser
+  const path = require('path'); //=> https://www.npmjs.com/package/path
+  const passport = require('passport'); //=> https://www.npmjs.com/package/passport
+
+
+/*
+Config
+*/
+
+ // Declarations
+  const server = express();
+  const port = process.env.PORT;
+
+ // Server class
+    class ServerClass {
+
+    init() {
+
+    // Static path configuration
+    server.set( 'views', __dirname + '/www' );
+    server.use( express.static(path.join(__dirname, 'www')) );
+
+    //=> Body-parser
+    server.use(bodyParser.json({limit: '10mb'}));
+    server.use(bodyParser.urlencoded({ extended: true }));
+
+    //=> Use CookieParser to setup serverside cookies
+    server.use(cookieParser(process.env.COOKIE_SECRET));
+
+    // Start server
+    this.launch();
+  };
+
+
+    routes() {
+    };
+
+
+    launch() {
+
+      // Init Routers
+      this.routes();
+
+      // Launch server
+      server.listen(port, () => console.log(`Server is running on port ${port}`))
+    };
+  }
+
+  /*
+  Start
+  */
+  new ServerClass().init();
